820384f3d30aaff1cae1177af1eecac36b658cf5
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-07-06 10:57:44 -0300

    :white_check_mark: test: add ui Test and golden test widget

--- test/domain/utils/stubs.dart
+++
@@ -1,3 +0,0 @@
-import 'package:tractian_challenge_one/domain/entities/company.dart';
-
-final List<Company> companyMock = [Company(id: "1", name: "Tractian")];
---
+++ test/stubs.dart
@@ -0,0 +1,10 @@
+import 'package:tractian_challenge_one/domain/entities/company.dart';
+
+final List<Company> companyMock = [
+  Company(id: "1", name: "Jaguar"),
+  Company(id: "2", name: "Tobias"),
+  Company(id: "3", name: "Apex"),
+];
+
+final Company companyMock1 =   Company(id: "1", name: "Jaguar");
+
